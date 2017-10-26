33305238314082e56c33eb69a7451b7c5e1b2c9d
Christian de Botton <cdebotton@gmail.com>
2017-10-25 21:13:35 -0400

    Correctly setup stylelint

--- admin/src/components/atoms/Page.tsx
+++ admin/src/components/atoms/Page.tsx
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const Page = styled.div`
+  position: absolute;
   display: flex;
+  width: 20px;
+  transform: translateX(50px);
 `;
 
 export default Page;
